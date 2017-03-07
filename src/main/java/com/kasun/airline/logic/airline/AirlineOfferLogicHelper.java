1614e253ec58d5e81a69c930814cb19cd7da82cb
Kasun Kariyawasam <kasunsk@gmail.com>
2017-03-07 18:29:27 +0530

    available offer unit test

--- src/main/java/com/kasun/airline/logic/airline/AirlineOfferLogicHelper.java
+++ src/main/java/com/kasun/airline/logic/airline/AirlineOfferLogicHelper.java
@@ -1,6 +1,7 @@
 package com.kasun.airline.logic.airline;
 
 import com.kasun.airline.common.dto.ServiceRequest;
+import com.kasun.airline.common.execption.ServiceRuntimeException;
 import com.kasun.airline.dao.airline.AirlineDao;
 import com.kasun.airline.model.airline.AirlineOfferModel;
 import com.kasun.airline.model.airline.Route;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * This helper class will provide common methods for Airline Logic classes.
+ * This helper class will provide common helper methods for Airline Logic classes.
  */
 @Component
 public class AirlineOfferLogicHelper {
@@ -21,10 +22,18 @@ public class AirlineOfferLogicHelper {
     private UserService userService;
 
     protected AirlineOfferModel loadOfferByRout(Route airlineRout) {
+
+        if (airlineRout == null || airlineRout.getFrom() == null || airlineRout.getTo() == null) {
+            throw new ServiceRuntimeException("Invalid airline route");
+        }
         return airlineDao.loadOfferByRoute(airlineRout.getFrom(), airlineRout.getTo());
     }
 
     protected void authenticateApplicant(String applicantId) {
+
+        if (applicantId == null || applicantId.isEmpty()) {
+            throw new ServiceRuntimeException("Invalid applicant id");
+        }
         userService.authenticateUser(new ServiceRequest<>(applicantId));
     }
 }
--- src/test/java/com/kasun/airline/logic/airline/AirlineOfferLogicHelperTest.java
+++ src/test/java/com/kasun/airline/logic/airline/AirlineOfferLogicHelperTest.java
@@ -1,6 +1,94 @@
 package com.kasun.airline.logic.airline;
 
 
+import com.kasun.airline.common.execption.ServiceRuntimeException;
+import com.kasun.airline.dao.airline.AirlineDao;
+import com.kasun.airline.model.airline.AirlineOfferModel;
+import com.kasun.airline.model.airline.Route;
+import com.kasun.airline.service.user.UserService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.FileAssert.fail;
+
 
 public class AirlineOfferLogicHelperTest {
+
+    @InjectMocks
+    AirlineOfferLogicHelper logicHelper = new AirlineOfferLogicHelper();
+
+    @Mock
+    AirlineDao airlineDao;
+
+    @Mock
+    UserService userService;
+
+    @BeforeMethod(alwaysRun = true)
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void loadAirlineOfferByRouteInvalidInputTest() {
+        logicHelper.loadOfferByRout(null);
+    }
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void loadAirlineOfferByRouteInvalidInputDestinationTest() {
+        logicHelper.loadOfferByRout(new Route());
+    }
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void loadAirlineOfferByRouteInvalidOriginTest() {
+        Route route = getRoute(null, "XXX");
+        logicHelper.loadOfferByRout(route);
+    }
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void loadAirlineOfferByRouteInvalidDestinationTest() {
+        Route route = getRoute("YYY", null);
+        logicHelper.loadOfferByRout(route);
+    }
+
+    @Test
+    public void loadAirlineOfferByRouteTest() {
+        Route route = getRoute("YYY", "XXX");
+        AirlineOfferModel airlineOfferModel = new AirlineOfferModel();
+        when(airlineDao.loadOfferByRoute("YYY", "XXX")).thenReturn(airlineOfferModel);
+        AirlineOfferModel resultAirlineOfferModel = logicHelper.loadOfferByRout(route);
+        assertEquals(resultAirlineOfferModel, airlineOfferModel);
+    }
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void authenticateApplicantInputValidateNullTest(){
+        logicHelper.authenticateApplicant(null);
+    }
+
+
+    @Test(expectedExceptions = ServiceRuntimeException.class)
+    public void authenticateApplicantInputValidateEmptyTest(){
+        logicHelper.authenticateApplicant("");
+    }
+
+    @Test
+    public void authenticateApplicantInputValidateTest(){
+
+        try {
+            logicHelper.authenticateApplicant("applicantId");
+        } catch (ServiceRuntimeException ex) {
+            fail();
+        }
+    }
+
+    private Route getRoute(String origin, String destination) {
+        Route route = new Route();
+        route.setTo(destination);
+        route.setFrom(origin);
+        return route;
+    }
 }
